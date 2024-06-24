f8ddff1b25455185fc7f460be3dae0ff4c7395fe
aydansn <97697518+aydansn@users.noreply.github.com>
2024-06-24 12:04:45 +0400

    Add files via upload

---
+++ Loops/script.js
@@ -0,0 +1,78 @@
+for(let i=1; i<=50; i++){
+    console.log(i, `Vetendash ${i} ses verdi `);
+}
+
+
+const populasiyalar = [4567,9865,2972];
+const populasiyalar2 = [];
+const hesabla = populasiyalar2.map((populasiyalar) => {
+  const  worldPopulasia = populasiyalar*100/hesabla;
+  faiz = populasiyalar2.push(worldPopulasia);
+  return faiz;
+});
+console.log((hesabla));
+
+
+const qonshular =  [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden',
+    'Russia']];
+    for(i=0; i<=qonshular.length; i++){
+        for(j=0; j=qonshular.length[i]; j++){
+            console.log(qonshular);
+        }
+    }
+
+    const hesablar = [22, 295, 176, 440, 37, 105, 10, 1100, 86,
+        52];
+        const servisHaqqi=[];
+        const umumiHesab=[];
+        const umumiHesabHesabla=()=>{
+            sum = 0;
+            const umumiHesab = sum += hesablar;
+
+        };
+
+
+//-------TASK-2-------   
+for( i=1; i<=20; i++){
+    console.log(i);
+}     
+
+let array3 = [[1,2],[3,4],[5,6]];
+
+for(let i=0; i<array3.length;i++){
+   // console.log(i);
+    for (let j = 0; j < array3[i].length; j++) {
+        console.log(array3[i][j]);
+    }
+}
+const array = [1,2,3,4,5,6];
+for(let i=1; i<=array.length;i++){
+    if(i%2==0){
+        console.log(i,"cut ededler");
+    }
+    else{
+        console.log(i,"tek ededler");
+    }
+}
+
+for (let i = 7; i <= 31; i++) {
+    if (i % 2 == 0) {
+        console.log(i + "-cutdur");
+    }
+    else {
+        console.log(i + "-tekdir");
+    }
+} 
+
+for(let i = 1; i<=45; i++){
+    console.log(i);
+    if(i % 3 == 0){
+        console.log("Fizz");
+    }
+     if(i%5==0){
+        console.log("Buzz");
+    }
+    if(i % 3 == 0 && i%5==0){
+        console.log("FizzBuzz")
+    }
+}
